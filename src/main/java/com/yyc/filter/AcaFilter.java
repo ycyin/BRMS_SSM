e51b2dcb3a076c90b86a2fda80cc4398d5632c60
yyc <34782655@qq.com>
2020-05-20 21:30:05 +0800

    修改代码规范

--- src/main/java/com/yyc/filter/AcaFilter.java
+++ src/main/java/com/yyc/filter/AcaFilter.java
@@ -1,33 +1,37 @@
 package com.yyc.filter;
 
-import org.springframework.core.annotation.Order;
- 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
  
+
 /**
  * 跨域的设置问题
  * 参考：https://blog.csdn.net/qq_39313596/article/details/82592809
- * @author tonywu
- * @version v1.0.0
+ *
+ * @ClassName:  AcaFilter
+ * @Description: 跨域过滤器
+ * @author: yyc
+ *
+ * 常用注解
+ * /@Component  //在启动类加了@ServletComponentScan无需加这个了(springboot)
+ * /@Order(5)  //设置优先级加载
+ * /@ServletComponentScan  //加载启动类上了(springboot)
+ * /@WebFilter(urlPatterns = "/*",filterName = "ACAFilter")
  */
-//
-////@Component   //在启动类加了@ServletComponentScan无需加这个了(springboot)
-//@Order(5)//设置优先级加载
-////@ServletComponentScan  //加载启动类上了(springboot)
-//@WebFilter(urlPatterns = "/*",filterName = "ACAFilter")
-public class ACAFilter implements Filter {
- 
+public class AcaFilter implements Filter {
+    private static Logger logger = LoggerFactory.getLogger(AcaFilter.class);
+
     /**
      * 初始化
      * @param filterConfig FilterConfig
-     * @throws ServletException
      */
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
+    public void init(FilterConfig filterConfig) {
     }
  
     /**
@@ -46,21 +50,23 @@ public class ACAFilter implements Filter {
         if(origin == null) {
             origin = req.getHeader("Referer");
         }
-        resp.setHeader("Access-Control-Allow-Origin", origin);//这里不能写*，*代表接受所有域名访问，如写*则下面一行代码无效。谨记
-        resp.setHeader("Access-Control-Allow-Credentials", "true");// 携带Cookie
+        // 这里不能写*，*代表接受所有域名访问，如写*则下面一行代码Allow-Credentials无效。谨记
+        resp.setHeader("Access-Control-Allow-Origin", origin);
+        // 允许携带Cookie
+        resp.setHeader("Access-Control-Allow-Credentials", "true");
         resp.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
         resp.setHeader("Access-Control-Max-Age", "3600");
-        //允许前端发送的请求头类型，比如 axios.defaults.headers.common['Token'] =  getToken(); 这里就写上Token
+        // 允许前端发送的请求头类型，比如 axios.defaults.headers.common['Token'] =  getToken(); 这里就写上Token
         resp.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type, Accept, Origin,Token");
 
-        if (req.getMethod().equals("OPTIONS")) {
+        if ("OPTIONS".equals(req.getMethod())) {
             resp.setStatus(200);
             return;
         }
 
         chain.doFilter(req, resp);
 
-        System.out.println("ACAFilter："+"to access control allow origin");
+        logger.info("ACAFilter："+"to access control allow origin");
     }
  
     /**
